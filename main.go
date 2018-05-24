bd78a745e53510ddeccce2337b18c5e41fa53f7d
erleene <erleene@beamery.com>
2018-05-24 17:01:02 +0100

    building out input fields

--- main.go
+++ main.go
@@ -10,20 +10,21 @@ func main() {
 func LoadApp() {
 	app := tview.NewApplication()
 
-	//heightField := tview.NewInputField().SetLabel("Height")
+	heightField := tview.NewInputField().SetLabel("Height")
+	weightField := tview.NewInputfield().SetLabel("Weight")
 
 	form := tview.NewForm().
-		AddInputField("height", "", 10, nil, nil).
-		AddInputField("Weight", "", 10, nil, nil).
-		AddButton("Calculate BMI", nil).
-		AddButton("Exit", func() {
-			app.Stop()
-		}).
-		SetCancelFunc(func() {
-			app.Stop()
-		})
+		AddInputField(heightField, "", 10, nil, nil).
+		// 	AddInputField("Weight", "", 10, nil, nil).
+		// 	AddButton("Calculate BMI", nil).
+		// 	AddButton("Exit", func() {
+		// 		app.Stop()
+		// 	}).
+		// 	SetCancelFunc(func() {
+		// 		app.Stop()
+		// 	})
 
-	form.SetBorder(true).SetTitle("BMI Calculator").SetTitleAlign(tview.AlignLeft)
+		form.SetBorder(true).SetTitle("BMI Calculator").SetTitleAlign(tview.AlignLeft)
 	if err := app.SetRoot(form, true).SetFocus(form).Run(); err != nil {
 		panic(err)
 	}
